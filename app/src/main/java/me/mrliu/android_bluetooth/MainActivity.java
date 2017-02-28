3128526217cb220e7fec5cda7afb1a7842b5cf37
LiuKang <583665989@qq.com>
2017-02-28 17:49:23 +0800

    获取已配对设备、搜索蓝牙设备

--- app/src/main/java/me/mrliu/android_bluetooth/MainActivity.java
+++ app/src/main/java/me/mrliu/android_bluetooth/MainActivity.java
@@ -1,15 +1,23 @@
 package me.mrliu.android_bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnOpen;
+    private Button btnGetDevices, btnFindDevices;
 
     private BluetoothAdapter mBluetoothAdapter;
 
@@ -26,10 +34,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void setListeners() {
         btnOpen.setOnClickListener(this);
+        btnGetDevices.setOnClickListener(this);
+        btnFindDevices.setOnClickListener(this);
     }
 
     private void setViews() {
         btnOpen = (Button) findViewById(R.id.btn_open);
+        btnGetDevices = (Button) findViewById(R.id.btn_get_devices);
+        btnFindDevices = (Button) findViewById(R.id.btn_find_devices);
     }
 
     @Override
@@ -38,6 +50,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_open:
                 openBluetooth();
                 break;
+            case R.id.btn_get_devices:
+                getDevices();
+                break;
+            case R.id.btn_find_devices:
+                findDevices();
+                break;
+        }
+    }
+
+    /**
+     * 搜索蓝牙设备
+     */
+    private void findDevices() {
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+        mBluetoothAdapter.startDiscovery();
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d("TAG", device.getName()+", "+device.getAddress());
+            }
+        }
+    };
+
+
+    /**
+     * 获取蓝牙设备
+     */
+    private void getDevices() {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size()>0) {
+            for (BluetoothDevice device : pairedDevices) {
+                Log.d("TAG", device.getName()+", "+device.getAddress());
+            }
         }
     }
 
